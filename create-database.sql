54e4dfacfbb4bca7c8b9d9fdb6a2aad443e2c5c6
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-12-17 17:35:10 +0545

    first commit

---
+++ create-database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE db;
+
+-- OR
+CREATE SCHEMA db;
+
+-- Database and schema is same in MySQL
+-- 
+-- A new database will always have public schema by default.
+create database db;
+
+-- This will create a new schema inside existing database which will sit along side a public schema.
+create schema db;
---
+++ drop-database.sql
@@ -0,0 +1,11 @@
+DROP DATABASE db;
+
+-- OR
+DROP SCHEMA db;
+
+-- 
+-- To drop a database
+drop database db;
+
+-- To drop a schema
+drop schema db;
